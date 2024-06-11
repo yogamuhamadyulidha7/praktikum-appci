3bc1c3f0de340d55aa674481256c11a289304342
yogamuhamadyulidha7 <yogayulidha@gmail.com>
2024-06-11 16:29:26 +0700

    Codeigniter

--- application/controllers/Daftar.php
+++ application/controllers/Daftar.php
@@ -1,17 +1,19 @@
 <?php
 
 use Orm\User;
+
 class Daftar extends CI_Controller
 {
-    public function user($name, $password){
+    public function user($name, $password)
+    {
         // echo 'hallo';
         $user = new User();
-        $user->username=$name;
-        $user->password=$password;
-       if($user->save()){
-        echo "user dengan nama ".$name." berhasil";
-       } else {
+        $user->username = $name;
+        $user->password = $password;
+        if ($user->save()) {
+            echo "user dengan nama " . $name . " berhasil";
+        } else {
             echo "Error";
-       }
+        }
     }
 }
--- migrations/20240605095114_init.php
+++ migrations/20240605095114_init.php
@@ -8,26 +8,26 @@ final class Init extends AbstractMigration
     protected function up(): void
     {
         $this->table('user')
-        ->addColumn('id', 'integer', ['autoincrement' => true])
-        ->addColumn('username', 'string')
-        ->addColumn('password', 'string')
-        ->addColumn('created_at', 'datetime')
-        ->addColumn('updated_at', 'datetime', ['null' => true])
-        ->addIndex('username', Index::TYPE_UNIQUE)
-        ->create();
+            ->addColumn('id', 'integer', ['autoincrement' => true])
+            ->addColumn('username', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime', ['null' => true])
+            ->addIndex('username', Index::TYPE_UNIQUE)
+            ->create();
 
-        $this->insert('user',[
+        $this->insert('user', [
             [
                 'username' => 'yoga',
                 'password' => '123',
                 'created_at' => date('y-m-d'),
             ]
-            ]);
-}
+        ]);
+    }
 
     protected function down(): void
     {
         $this->table('user')
-        ->drop();
+            ->drop();
     }
 }
